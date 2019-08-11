ec74343316b800dd3bcf911fe2a9194a18e2fbe2
Lucas Akira Uehara <akira@MacBook-Pro-de-Lucas.local>
2019-08-11 11:19:42 -0300

    Updates

---
+++ precache-manifest.41f92cc9920ee11d5056b2e2b5b09b8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79e6c7032c2aef92ef52f8312415d6fd",
+    "url": "/whatsapp-helper-app/index.html"
+  },
+  {
+    "revision": "12c91c2f3febcff18471",
+    "url": "/whatsapp-helper-app/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "c43c88bc7bc41e743853",
+    "url": "/whatsapp-helper-app/static/css/main.6f17fa73.chunk.css"
+  },
+  {
+    "revision": "12c91c2f3febcff18471",
+    "url": "/whatsapp-helper-app/static/js/2.4dee1d50.chunk.js"
+  },
+  {
+    "revision": "c43c88bc7bc41e743853",
+    "url": "/whatsapp-helper-app/static/js/main.5cc7652f.chunk.js"
+  },
+  {
+    "revision": "86df54910088fe938431",
+    "url": "/whatsapp-helper-app/static/js/runtime~main.2d684a5a.js"
+  }
+]);
